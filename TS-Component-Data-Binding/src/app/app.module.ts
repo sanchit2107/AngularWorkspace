75a67fd37618b20c9b61128785c7857f22744a51
sanchit2107 <sanchit2107@gmail.com>
2020-05-16 17:44:26 +0530

    Data Binding Angular Demo

---
+++ TS-Component-Data-Binding/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { InputFormComponent } from './input-form/input-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InputFormComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
